54a5cbe434153811097dbdfe7cb6393578fabd3e
Mikey O'Brien <hmobrienv@gmail.com>
2018-07-11 12:02:50 -0700

    add extra keybinds and modes, edit agenda views

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,7 +13,7 @@
  '(org-tags-column -100)
  '(package-selected-packages
 	 (quote
-		(solarized-theme auto-compile org-bullets evil use-package))))
+		(org-pomodoro solarized-theme auto-compile org-bullets evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
