85d718cec91f737212d80adf47101b3f250a523f
Mikey O'Brien <hmobrienv@gmail.com>
2018-08-29 10:42:12 -0700

    add logic to control org-mode ellipsis

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,10 +10,11 @@
  '(custom-safe-themes
 	 (quote
 		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(ledger-clear-whole-transactions 5 t)
  '(org-tags-column -100)
  '(package-selected-packages
 	 (quote
-		(org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package))))
+		(ledger-mode org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
