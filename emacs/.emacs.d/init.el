b2bf1b332a390e4517725de46630a262099666d5
Mikey O'Brien <hmobrienv@gmail.com>
2019-01-23 10:23:25 -0600

    update configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,10 +11,12 @@
 	 (quote
 		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(ledger-clear-whole-transactions 5 t)
+ '(org-journal-dir "~/Nextcloud/journal/")
  '(org-tags-column -100)
  '(package-selected-packages
 	 (quote
-		(ledger-mode org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package))))
+		(neotree doom-themes wakatime-mode anaconda-mode org-projectile projectile ledger-mode org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package)))
+ '(wakatime-python-bin nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
