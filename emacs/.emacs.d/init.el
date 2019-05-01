605733f86bd557a371516af5a813ab2191db1734
Hugh OBrien <hmobrienv@gmail.com>
2019-05-01 18:11:22 -0500

    Move emacs from nextcloud

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,11 +11,12 @@
 	 (quote
 		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(ledger-clear-whole-transactions 5 t)
- '(org-journal-dir "~/Nextcloud/journal/")
+ '(org-agenda-files nil)
+ '(org-journal-dir "~/Documents/journal/")
  '(org-tags-column -100)
  '(package-selected-packages
 	 (quote
-		(neotree doom-themes wakatime-mode anaconda-mode org-projectile projectile ledger-mode org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package)))
+		(exec-path-from-shell rust-mode neotree doom-themes wakatime-mode anaconda-mode org-projectile projectile ledger-mode org-journal org-pomodoro solarized-theme auto-compile org-bullets evil use-package)))
  '(wakatime-python-bin nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
