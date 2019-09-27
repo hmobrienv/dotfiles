6d38ed0f3cf94294664397a86d03d297124caf72
Mikey O'Brien <hmobrienv@gmail.com>
2019-09-26 21:29:06 -0500

    Updated install script added spacemacs

--- install.sh
+++ install.sh
@@ -3,30 +3,36 @@
 export SRC_DIR=$(cd "$(dirname "$0")/.."; pwd)
 echo $SRC_DIR
 
-if [ ! -L ~/.vimrc ]; then
+if [ ! -L "$HOME/.vimrc" ]; then
     echo "Linking .vimrc"
-    ln -s $SRC_DIR/dotfiles/.vimrc ~/.vimrc
+    ln -s $SRC_DIR/dotfiles/.vimrc $HOME/.vimrc
 else
     echo "vim configuration already installed"
 fi
 
-if [ ! -L ~/.tmux.conf ]; then
+if [ ! -L "$HOME/.tmux.conf" ]; then
     echo "Linking .tmux.conf"
-    ln -s $SRC_DIR/dotfiles/.tmux.conf ~/.tmux.conf
+    ln -s $SRC_DIR/dotfiles/.tmux.conf $HOME/.tmux.conf
 else
     echo "tmux configuration already installed"
 fi
 
-if [ ! -d ~/.tmux/plugins/tpm ]; then
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     echo "Cloning tmux-plugins"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 else 
     echo "tmux plugins already installed"
 fi
 
-if [ ! -L ~/.emacs.d ]; then
+if [ ! -L "$HOME/.emacs.d" ]; then
     echo "Linking emacs directory"
-    ln -s $SRCDIR/dotfiles/spacemacs ~/.emacs.d
+    ln -s $SRC_DIR/dotfiles/spacemacs $HOME/.emacs.d
+    ln -s $SRC_DIR/dotfiles/.spacemacs $HOME/.spacemacs
+    
+    echo "Checking out development branch"
+    (cd $HOME/.emacs.d && git checkout develop)
 else
     echo "Spacemacs already linked"
 fi
+
+
