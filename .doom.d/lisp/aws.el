d3972229e4c9d2fdfad96a4d1b1f9ebabe26358e
Mikey O'Brien <mobrien@vectra.ai>
2021-01-08 16:53:28 -0600

    move between awstk buffers

--- .doom.d/lisp/aws.el
+++ .doom.d/lisp/aws.el
@@ -10,11 +10,10 @@
 
 (defun awstk-call-aws (&rest args)
   (awstk-clear-buffer)
-  (apply #'call-process awscli-executable nil process-buf nil (append '("--profile" "saas-dataeng-dev") args)))
+  (apply #'call-process awscli-executable nil process-buf nil (append '("--profile" profile) args)))
 
-(defun awstk-list-buckets ()
-  (interactive)
-  (awstk-call-aws "s3api" "list-buckets" "--query" "Buckets[].Name" "--output" "json" "--profile" profile)
+(defun awstk--update-buckets ()
+  (awstk-call-aws "s3api" "list-buckets" "--query" "Buckets[].Name" "--output" "json")
   (let ((buckets (with-current-buffer process-buf
                    (goto-char (point-min))
                    (json-parse-buffer))))
@@ -23,30 +22,36 @@
       (erase-buffer)
       (dotimes (i (length buckets))
         (insert (format "%s\n" (elt buckets i)))))
-    (pop-to-buffer "awstk:s3-buckets")
+    (display-buffer "awstk:s3-buckets" '(display-buffer-same-window))
     (goto-char (point-min))
     (awstk-s3-bucket-mode)))
+
+(defun awstk-list-buckets (&optional refresh)
+  (interactive)
+  (if (or refresh (not (get-buffer "awstk:s3-buckets")))
+      (awstk--update-buckets)
+    (display-buffer "awstk:s3-buckets" '(display-buffer-same-window))))
 
 (defun awstk-s3-info ()
   (interactive)
   (let ((bucket-name (string-trim (thing-at-point 'line))))
     (awstk-call-aws "s3api" "list-objects" "--bucket" bucket-name)
     (let ((objects (gethash "Contents" (with-current-buffer process-buf
-                     (goto-char (point-min))
-                     (json-parse-buffer)))))
+                                         (goto-char (point-min))
+                                         (json-parse-buffer)))))
       (with-current-buffer (get-buffer-create "awstk:s3-objects")
         (setq buffer-read-only nil)
         (erase-buffer)
+        (insert "bucket: " bucket-name "\n\n")
         (dotimes (i (length objects))
-          (insert (format "%s\n"(gethash "Key" (elt objects i)))))
-        (pop-to-buffer-same-window "awstk:s3-objects")
-        (goto-char (point-min))
-        (awstk-s3-object-mode)))))
+          (insert (gethash "Key" (elt objects i)) "\n"))
+        (switch-to-buffer "awstk:s3-objects")))))
 
 (defvar awstk-s3-bucket-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map special-mode-map)
     (define-key map (kbd "i") 'awstk-s3-info)
+    (define-key map "r" (lambda () (interactive (awstk-list-buckets t))))
     (define-key map (kbd "q") 'previous-buffer)
     (define-key map (kbd "j") 'next-line)
     (define-key map (kbd "k") 'previous-line)
@@ -67,12 +72,19 @@
 (defvar awstk-s3-object-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map awstk-s3-bucket-mode-map)
+    (define-key map "g" 'awstk-s3-get-object)
+    (define-key map (kbd "q") (lambda () (interactive) (pop-to-buffer-same-window "awstk:s3-buckets")))
     map)
   "Keymap for `awstk-s3-object-mode'.")
 
-(define-derived-mode awstk-s3-object-mode awstk-s3-bucket-mode-map "S3 Object Mode"
+(define-derived-mode awstk-s3-object-mode awstk-s3-bucket-mode "S3 Object Mode"
   "Major mode for handling a objects from a bucket.")
 
+;;;###autoload (autoload 'awstk "awstk" nil t)
+(transient-define-prefix awstk ()
+  "Transient for awstk."
+  :man-page "aws"
+  ["AWS"
+   ("s" "S3" awstk-list-buckets)])
 
-(awstk-list-buckets)
 (provide 'awstk)
